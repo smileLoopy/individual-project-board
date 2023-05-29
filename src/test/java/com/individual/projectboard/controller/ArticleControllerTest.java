5bff61c271589f1aa0ed227b8f87b4e07253f83e
smile.loopys2 <smile.loopys2@gmail.com>
2023-05-30 00:13:00 +0900

    #16- 컨트롤러 테스트 업데이트 미디어 타입 검사 방법을 d841e32 에서 확인했기 때문에, 나머지 뷰도 동일하게 작동할 것으로 예상하고 미리 테스트 메소드 업데이트

--- src/test/java/com/individual/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/individual/projectboard/controller/ArticleControllerTest.java
@@ -39,7 +39,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -53,7 +53,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -65,7 +65,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
